0244259e652172d51e15394eff39fae8fe99b822
Javi Martinez <javimartinezap@gmail.com>
2021-11-21 00:47:20 +0100

    feature/goclient
    Changes:
    
    - Fixed a minor issue related with ClientMock builder.

--- goclient/clientmock.go
+++ goclient/clientmock.go
@@ -5,29 +5,29 @@ import (
 )
 
 type httpClientMock struct {
-	getFn    func(url string, headers http.Header) (*response, error)
-	postFn   func(url string, headers http.Header, body interface{}) (*response, error)
-	putFn    func(url string, headers http.Header, body interface{}) (*response, error)
-	patchFn  func(url string, headers http.Header, body interface{}) (*response, error)
-	deleteFn func(url string, headers http.Header) (*response, error)
+	GetFn    func(url string, headers http.Header) (*response, error)
+	PostFn   func(url string, headers http.Header, body interface{}) (*response, error)
+	PutFn    func(url string, headers http.Header, body interface{}) (*response, error)
+	PatchFn  func(url string, headers http.Header, body interface{}) (*response, error)
+	DeleteFn func(url string, headers http.Header) (*response, error)
 }
 
 func (hcm *httpClientMock) Get(url string, headers http.Header) (*response, error) {
-	return hcm.getFn(url, headers)
+	return hcm.GetFn(url, headers)
 }
 
 func (hcm *httpClientMock) Post(url string, headers http.Header, body interface{}) (*response, error) {
-	return hcm.postFn(url, headers, body)
+	return hcm.PostFn(url, headers, body)
 }
 
 func (hcm *httpClientMock) Put(url string, headers http.Header, body interface{}) (*response, error) {
-	return hcm.putFn(url, headers, body)
+	return hcm.PutFn(url, headers, body)
 }
 
 func (hcm *httpClientMock) Patch(url string, headers http.Header, body interface{}) (*response, error) {
-	return hcm.patchFn(url, headers, body)
+	return hcm.PatchFn(url, headers, body)
 }
 
 func (hcm *httpClientMock) Delete(url string, headers http.Header) (*response, error) {
-	return hcm.deleteFn(url, headers)
+	return hcm.DeleteFn(url, headers)
 }
